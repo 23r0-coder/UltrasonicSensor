12f356cc03c10e98b56d43c2729a130d0eed51ec
23_ϱ <84030511+23r0-coder@users.noreply.github.com>
2022-05-13 09:50:25 -0600

    Add files via upload

---
+++ ProjectoFinal/lcd.py
@@ -0,0 +1,5 @@
+from pythonArduinoConnection import *
+
+# Defining the pins that will be used for the LCD.
+LiquidCrystal  = (3,4,7,8)
+
---
+++ ProjectoFinal/main.py
@@ -0,0 +1,15 @@
+import time
+import warnings
+from pythonArduinoConnection import *
+import pulsein
+import lcd
+
+
+#Main process
+while True:
+    #warnings.Start_Trigger()
+    #Trial values
+    warnings.Warnings(10)
+    # Warnings(15)
+    # Warnings(30)
+board.exit()
